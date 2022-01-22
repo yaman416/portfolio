6f43cbe8d56e1ce061d1bb3be0e3b07ec1d4f60f
Yaman Gurung <yamangurung4106@gmail.com>
2022-01-22 16:51:27 +1100

    new page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Works from "./components/works/Works";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
